d59f315206cd35756b25535144e51c8bf1d906e6
stre1ok <strelok-1111@yandex.ru>
2022-05-16 10:45:32 +0300

    test

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,15 @@
 import telebot
 
 
-bot = telebot.Telebot('5307565681:AAF2rQrwXKPOAWuXG_WQU6YhbWGE4q6-M6M')
+bot = telebot.TeleBot('5307565681:AAF2rQrwXKPOAWuXg_WQU6YhbWGE4q6-M6M')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message()
+    bot.send_message(message.chat.id, 'Hello')
+
+@bot.message_handler(message)
+def obligation_calculation():
+    """Calculation"""
+    if message.text == '/calc':
+        bot.send_message(message)
+bot.polling(none_stop=True)
