b328d67770767f5f387506885d54d9bcf1d9e48f
stre1ok <strelok-1111@yandex.ru>
2022-05-16 10:14:12 +0300

    new

---
+++ venv/main.py
@@ -0,0 +1,10 @@
+import telebot
+
+
+bot = telebot.Telebot('5307565681:AAF2rQrwXKPOAWuXG_WQU6YhbWGE4q6-M6M')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message()
+
+    ###
