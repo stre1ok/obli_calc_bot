3f9ade83e8d6ba174696aa9215657c35def85a9c
stre1ok <strelok-1111@yandex.ru>
2022-05-16 13:31:09 +0300

    Update main.py
    try1

--- venv/main.py
+++ venv/main.py
@@ -1,17 +1,21 @@
 import telebot
+from telebot import types
 
 
 bot = telebot.TeleBot('5307565681:AAF2rQrwXKPOAWuXg_WQU6YhbWGE4q6-M6M')
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def start_message(message):
     bot.send_message(message.chat.id, 'Hello')
 
 @bot.message_handler(message)
 def obligation_calculation():
     """Calculation"""
     if message.text == '/calc':
-        bot.send_message(message)
+        bot.send_message(message.from_user.id, 'Введите стоимость облигации(в валюте)')
+        price = message.text
+        bot.send_message(message.from_user.id, 'Введите номинал облигации(в валюте)')
+        basePrice = message.text
 
 bot.polling(none_stop=True)
 
