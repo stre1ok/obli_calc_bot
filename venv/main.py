bdd5f3199d22c7c541c6cfe2978c4dbf3093066d
stre1ok <strelok-1111@yandex.ru>
2022-05-17 09:09:22 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -38,8 +38,9 @@ def what_bondCoupon(message):
 def what_periodInAge(message):
     global periodInAge
     periodInAge = float(message.text)
-    bot.send_message(message.from_user.id, f"Доходность от разницы номинала: {round((((price/basePrice*100)-100)*-1), 2)}%")
-    bot.send_message(message.from_user.id, f"Доходность от разницы номинала c учетом НДФЛ 13%: {round(((((price/basePrice*100)-100)*-1)*0.87), 2)}%")
+    bot.send_message(message.from_user.id, f"Доходность от разницы номинала: {round((((price/basePrice*100)-100)*-1), 2)}% \n
+Доходность от разницы номинала c учетом НДФЛ 13%: {round(((((price/basePrice*100)-100)*-1)*0.87), 2)}%")
+    #bot.send_message(message.from_user.id, f"Доходность от разницы номинала c учетом НДФЛ 13%: {round(((((price/basePrice*100)-100)*-1)*0.87), 2)}%")
     bot.send_message(message.from_user.id, f"Доходность от разницы номинала в год: {round((((((price/basePrice*100)-100)*-1)/(periodInAge//1+periodInAge%1/12*10))), 2)}%")
     bot.send_message(message.from_user.id, f"Доходность от разницы номинала в год с учетом НДФЛ 13%: {round((((((price/basePrice*100)-100)*-1)/(periodInAge//1+periodInAge%1/12*10))*0.87), 2)}%")
     bot.send_message(message.from_user.id, f"Общая доходность в год: {round((((((price/basePrice*100)-100)*-1)/(periodInAge//1+periodInAge%1/12*10))+bondCoupon), 2)}%")
